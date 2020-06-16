bac905aee8b550a232aba6c9f86f4237cfa6ccad
FN Otaibi <nsayemlil@hotmail.com>
2020-06-16 16:09:58 +0300

    fnoCalculation.js

--- calculator.js
+++ calculator.js
@@ -1,3 +1,10 @@
 // Define the numbers and the operation here
-
+const A = 8;
+const B = 4;
+const operation = "+";
 // Write if-statements here
+if (operation == "+") console.log(A + B);
+else if (operation == "-") console.log(A - B);
+else if (operation == "*") console.log(A * B);
+else if (operation == "%") console.log(A % B);
+else console.log "where is Math"
